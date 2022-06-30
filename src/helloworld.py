f608a7ae741cd9f294d5c10dda087747a40aaca2
HendricksCode <keithhendricks50@gmail.com>
2022-06-30 18:59:48 -0500

    added deckOfCards class

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,18 +1,34 @@
-def hello_world(city):
-    print(f'Hello World from {city}')
-    print('This is a test')
+import random
 
-def main():
-    hello_world('OKC')
+# def hello_world(city):
+#     print(f'Hello World from {city}')
+#     print('This is a test')
 
-def test():
-    print('This is another GitHub test, good luck')
+# def main():
+#     hello_world('OKC')
+
+# def test():
+#     print('This is another GitHub test, good luck')
     
-def test2():
-    print('The testing and practice continues!') 
+# def test2():
+#     print('The testing and practice continues!') 
 
 
-def test3():
-    print('Working by myself is borring but I need the practice.')
+# def test3():
+#     print('Working by myself is borring but I need the practice.')
    
-main()
+# main()
+
+
+class deckOfCards():
+    
+    def __init__(self):
+        suits = ["spades", "hearts", "dimonds", "clubs"]
+        self.number = random.randint(1, 10)
+        self.suit = random.choice(suits)
+    
+    def __repr__(self):
+        return f"You have been delt a {self.number} of {self.suit}"
+
+first_card = deckOfCards()
+print(first_card)
